367a054135f5ca052c569231bc841cfa3bc18f43
Rabi jackson .j <rabijacksonjohn@gmail.com>
2025-10-15 23:44:48 +0530

    Added:app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const PORT = 5050
+
+app.get('/',(req,res)=>{
+    res.send("Hello world")
+})
+
+app.listen(PORT,()=>{
+    console.log("The port is running on",PORT);
+    
+})
